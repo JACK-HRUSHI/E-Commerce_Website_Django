55750b1c1b0293daa4243a7c90b76b932d1fe1d8
JACK-HRUSHI <hrushikeshjagtap2002@gmail.com>
2022-02-19 20:45:53 +0530

    Done uploadation in database's contact and order table. It's very easy to work with database in django in some steps in models, views and admin pyfiles with providing method and action with csrf token in checkout pyfile's form tag

--- mac/shop/models.py
+++ mac/shop/models.py
@@ -38,3 +38,4 @@ class Orders(models.Model):
     city = models.CharField(max_length=111)
     state = models.CharField(max_length=111)
     zip_code = models.CharField(max_length=111)
+    phone = models.CharField(max_length=111, default="")
