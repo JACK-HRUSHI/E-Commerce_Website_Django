53f0e213aa1156ce4c0689ebebd55557575ab05c
JACK-HRUSHI <hrushikeshjagtap2002@gmail.com>
2022-02-20 11:30:19 +0530

    Validation for contact and checkout form with alert for contact form

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -33,6 +33,10 @@ def contact(request):
         desc = request.POST.get('desc', '')
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
+        thank = True
+
+        return render(request, 'shop/contact.html', {'thank': thank})
+
     return render(request, 'shop/contact.html')
 
 
@@ -51,6 +55,7 @@ def prodView(request, myid):
 
 def checkout(request):
     if request.method == "POST":
+        items_json = request.POST.get('itemsJson', '')
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
         add = request.POST.get('add1', '') + " " + request.POST.get('add2', '')
@@ -58,6 +63,11 @@ def checkout(request):
         state = request.POST.get('state', '')
         zip_code = request.POST.get('zip_code', '')
         phone = request.POST.get('phone', '')
-        order = Orders(name=name, email=email, add=add, city=city, state=state, zip_code=zip_code, phone=phone)
+        order = Orders(items_json=items_json, name=name, email=email, add=add, city=city, state=state, zip_code=zip_code, phone=phone)
         order.save()
+        thank = True
+        id = order.order_id
+
+        return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
+
     return render(request, 'shop/checkout.html')
