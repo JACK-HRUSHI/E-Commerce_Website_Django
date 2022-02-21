3bcddde3b956e18d085ca8a6fd640099771c2fa4
JACK-HRUSHI <hrushikeshjagtap2002@gmail.com>
2022-02-21 19:59:05 +0530

    Done With Creation Of Blog

--- mac/blog/urls.py
+++ mac/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='BlogHome'),
+    path("", views.index, name='blogHome'),
+    path("blogPost/", views.blogpost, name='blogPost')
 ]
