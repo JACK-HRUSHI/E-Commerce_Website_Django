c66f8b8248d23fa935c7199e31615cbdf7d57282
JACK-HRUSHI <hrushikeshjagtap2002@gmail.com>
2022-02-22 11:00:34 +0530

    Done With Creation Of Blog Model

--- mac/blog/admin.py
+++ mac/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Blogpost
+
+admin.site.register(Blogpost)
--- mac/blog/models.py
+++ mac/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+# Refer Model Field Reference Django for more info about RHS of below.
+
+#  Do changes in MAIN PROJECT --> SETTINGS.PY --> INSTALLED_APPS[] --> Replace 'shop' with 'blog.apps.BlogConfig' which is a class name( BlogConfig ) from apps.py file
+
+class Blogpost(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    head0 = models.CharField(max_length=500, default="")
+    chead0 = models.CharField(max_length=5000, default="")
+    head1 = models.CharField(max_length=500, default="")
+    chead1 = models.CharField(max_length=5000, default="")
+    head2 = models.CharField(max_length=500, default="")
+    chead2 = models.CharField(max_length=5000, default="")
+    pub_date = models.DateField(null=True)
+    thumbnail = models.ImageField(upload_to="blog/images", default="")
+
+    def __str__(self):
+
+        return self.title
