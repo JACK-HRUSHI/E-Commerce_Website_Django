e42772c8ee2b83df6883e20f436742d29b45f196
JACK-HRUSHI <hrushikeshjagtap2002@gmail.com>
2022-03-26 17:11:10 +0530

    Done with extra

--- mac/blog/views.py
+++ mac/blog/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
-from .models import Blogpost
+from .models import Blogpost, Suggest
 
 def index(request):
     myposts = Blogpost.objects.all()
     print(myposts)
     return render(request, 'blog/index.html', {'myposts': myposts})
 
+
 def blogpost(request, id):
     post = Blogpost.objects.filter(post_id=id)[0]
     print(post)
     return render(request, 'blog/blogpost.html', {'post': post})
+
+
+def suggest(request):
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        suggest = Suggest(name=name, email=email, phone=phone, desc=desc) # Contact() is a constructor of Contact Class
+        suggest.save()
+        thank = True
+
+        return render(request, 'blog/suggest.html', {'thank': thank})
+
+    return render(request, 'blog/suggest.html')
