66fc0a41d41e861766462228b93e867568cc662f
JACK-HRUSHI <hrushikeshjagtap2002@gmail.com>
2022-04-29 21:16:59 +0530

    horuko

--- mac/mac/urls.py
+++ mac/mac/urls.py
@@ -18,15 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
-    path('', views.index),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
+    path('', views.index)
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
