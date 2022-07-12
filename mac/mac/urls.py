21a3d4e753b43ac0e9f059af43f9c2b1b8ede0bb
JACK-HRUSHI <hrushikeshjagtap2002@gmail.com>
2022-07-12 21:00:30 +0530

    heruko

--- mac/mac/urls.py
+++ mac/mac/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
+from django.views.static import serve  #Heroku deployment
+from django.conf.urls import url  #Heroku deployment
 
 from . import views
 
@@ -27,7 +27,7 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
     path('', views.index),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),  #Heroku deployment
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),  #Heroku deployment
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
